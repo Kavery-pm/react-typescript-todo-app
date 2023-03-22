485ccb3b1b933521b19732380bb8797a29193b6d
kavery-pm <shiny.kavery@gmail.com>
2023-03-22 09:17:11 +0100

    added a todo type data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import './App.css';
 import Todos from './components/Todos';
-
+import Todo from './models/todo';
 function App() {
+  const todos =[
+    new Todo('Learn Typescript'),
+    new Todo('Learn React'),
+    new Todo('Learn React with typescript')
+
+  ]
   return (
     <div className="App">
-     <Todos items={['Learn React','learn next']}/>
+     <Todos items={todos}/>
     </div>
   );
 }
