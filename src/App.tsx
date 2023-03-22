d9342388a8538341e0bfa2d8a2308de593fbf376
kavery-pm <shiny.kavery@gmail.com>
2023-03-22 11:44:21 +0100

    added new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-
-import './App.css';
-import NewTodo from './components/NewTodo';
-import Todos from './components/Todos';
-import Todo from './models/todo';
+import { useState } from "react";
+import "./App.css";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 function App() {
-  const todos =[
-    new Todo('Learn Typescript'),
-    new Todo('Learn React'),
-    new Todo('Learn React with typescript')
+  const [todoItems, settodoItems] = useState<Todo[]>([]);
 
-  ]
-  const addTodoHandler = ()=>{
-    
-  }
+  const addTodoHandler = (todoItem: string) => {
+    const newTodo = new Todo(todoItem);
+    // settodoItems((existingTodo) => {
+    //   return existingTodo.concat(newTodo);
+    // });
+    settodoItems([newTodo,...todoItems]);
+  };
   return (
     <div className="App">
-       <NewTodo onAddText={addTodoHandler}/>
-     <Todos items={todos}/>
-    
+      <NewTodo onAddText={addTodoHandler} />
+      <Todos items={todoItems} />
     </div>
   );
 }
