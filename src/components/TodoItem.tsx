f036d6d683712a53e7550a6db6a56d8c8e27de74
kavery-pm <shiny.kavery@gmail.com>
2023-03-22 16:14:31 +0100

    added delete icon

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+
 import classes from './TodoItem.module.css';
+import * as React from 'react';
+import { FaRegTrashAlt } from 'react-icons/fa';
+
 const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+  return <li className={classes.item}>{props.text}  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; <FaRegTrashAlt style={{marginLeft:'200px'}}/> </li>
+  ;
 };
 export default TodoItem;
